4a73559754bb6f93b1c73ce9a99898aea5f5076d
zainabahmed <zainabahmedlll19@gmail.com>
2026-08-29 02:15:19 +0300

    added css to inbox & chat pages, improve both functionality

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router'
+import { Link, useParams } from 'react-router'
 import { conversations } from '../services/message'
 
 const Inbox = ({ user }) => {
+    const { recipientId } = useParams()
     const [conversationList, setConversationList] = useState([])
     const [loading, setLoading] = useState(true)
 
@@ -21,13 +22,18 @@ const Inbox = ({ user }) => {
         <div className="inbox">
             <h1 className="inbox-title">Direct Messages</h1>
 
+            <div className="message-search">
+            <span>⌕</span>
+            <input type="text" placeholder="Search messages..." />
+        </div>
+
             {conversationList.length === 0 ? (
                 <p className="empty-inbox">No conversations yet.</p>
             ) : (
                 <ul className="conversation-list">
                     {conversationList.map((convo) => (
-                        <li className="conversation-item" key={convo.roomId}>
-                            <Link className="conversation-link" to={`/chat/${convo.otherUser?._id}`}>
+                        <li   className={`conversation-item ${ recipientId === convo.otherUser?._id ? 'active' : ''  }`} key={convo.roomId}>
+                            <Link className="conversation-link" to={`/messages/${convo.otherUser?._id}`}>
                                 <img className="conversation-avatar"
                                     src={convo.otherUser?.avatar || 'https://placehold.co/50x50?text=%20'} width="50px"
                                     alt={convo.otherUser?.username}
